4912f08d3c4c27bcc8ffda3e82f6be82136e363d
LevNic <klacc.f@gmail.com>
2020-09-23 13:44:20 +0500

    Task 2

--- Geekshop/mainapp/views.py
+++ Geekshop/mainapp/views.py
@@ -1,14 +1,29 @@
 from django.shortcuts import render
 
 # Create your views here.
+links_menu = [
+    {'href': 'products_all', 'name': 'все'},
+    {'href': 'products_home', 'name': 'дом'},
+    {'href': 'products_office', 'name': 'офис'},
+    {'href': 'products_modern', 'name': 'модерн'},
+    {'href': 'products_classic', 'name': 'классика'},
+]
 
 
 def main(request):
-    return render(request, 'mainapp/index.html')
+    content = {
+
+    }
+
+    return render(request, 'mainapp/index.html', content)
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+
+    content = {
+        'links_menu': links_menu
+    }
+    return render(request, 'mainapp/products.html', content)
 
 
 def contacts(request):
